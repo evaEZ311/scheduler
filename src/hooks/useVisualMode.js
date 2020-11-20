90a23852d118b3fe5d073e98def521c0804f0b23
evaEZ <eva.jingzhao@gmail.com>
2020-11-19 21:48:48 -0500

    update the Mode and History to user setState

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,28 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (mode, replace = false){ 
-    if (replace === true) {
-      const currentMode = history.pop();
-      const updateHistory = history.filter((h) => h !== currentMode);
+  const transition = (newMode, replace) => {
+    if (!replace) {
+      setMode(mode);
+      setHistory([...history, newMode]);
     }
-    setMode(mode);
-    setHistory(prev => [...prev, mode]);
+    setMode(newMode);
   };
-
-  function back(){
-      if (history.length === 1){
-        return;
-      }
-      const currentMode = history.pop();
-      const updateHistory = history.filter((h) => h !== currentMode);
-      setHistory(updateHistory);
-
-      const prevMode = history[history.length-1];
-      setMode(prevMode);
-    
-  };
-
+  
+  const back = () => {
+    if (history.length === 1){
+      setMode(initial);
+    }
+    let prevHistory = history.slice(0, history.length - 1);
+    setHistory([...prevHistory]);
+    setMode(history[history.length-2]);
+  }
+   
   return { mode, transition, back };
 }
 
